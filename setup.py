736dbbbc7a7cbd173832504822593e254b5f73fb
Daniel Nakhimovich <danielnak@tutanota.com>
2022-07-31 15:21:26 -0400

    forgot to add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    packages=[
+        'execution_scene',
+        'grasp_planner',
+        'motion_planner',
+        'perception',
+        'scene',
+        'task_planner',
+        'utils',
+    ],
+    package_dir={'': 'scripts'},
+    install_requires=[
+        # 'moveit_commander',
+        'moveit_msgs'
+    ],
+)
+
+setup(**setup_args)
